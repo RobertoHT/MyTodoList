9b9cc1316a2035efc70274d02d350bf98ad516c7
Roberto Hernández Tamayo <hetr@praxis.com.mx>
2017-03-28 14:49:35 -0600

    Feature - Vista agregar nueva tarea
    -Se agrego el fragment e implementación de la vista agregar nueva tarea.

---
+++ app/src/main/java/micromaster/beginner/com/mytodolist/AddTaskDialogFragment.java
@@ -0,0 +1,42 @@
+package micromaster.beginner.com.mytodolist;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by praxis on 28/03/17.
+ */
+
+public class AddTaskDialogFragment extends DialogFragment {
+    public static AddTaskDialogFragment getInstance(String idTask){
+        Bundle bundle = new Bundle();
+        bundle.putString("ID_TASK", idTask);
+        AddTaskDialogFragment fragment = new AddTaskDialogFragment();
+        return fragment;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(R.layout.task_create_dialog);
+        builder.setTitle("Add a new task");
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        return builder.create();
+    }
+}
