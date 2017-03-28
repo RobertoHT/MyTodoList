8741e59bcee5b2865fd75a49624c63d185c0ee36
Roberto Hernández Tamayo <hetr@praxis.com.mx>
2017-03-28 14:48:19 -0600

    Feature - Vista principal
    -Se agregaron un recycler view y un floating button a la vista principal.
    -Se implementa los dos objetos en la clase respectiva.

--- app/src/main/java/micromaster/beginner/com/mytodolist/MainActivity.java
+++ app/src/main/java/micromaster/beginner/com/mytodolist/MainActivity.java
@@ -1,7 +1,12 @@
 package micromaster.beginner.com.mytodolist;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager mlinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mlinearLayoutManager);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(itemDecoration);
+        mRecyclerView.setHasFixedSize(true);
+
+        FloatingActionButton actionButton = (FloatingActionButton) findViewById(R.id.floating_action_btn);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
     }
 }
