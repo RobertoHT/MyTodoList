da2276b1fcbff4dcb2317b3d9dfd59af3d9d757b
Roberto Hernández Tamayo <hetr@praxis.com.mx>
2017-03-31 10:20:49 -0600

    Feature - Loader
    -Se agregó el loader a la aplicación.

--- app/src/main/java/micromaster/beginner/com/mytodolist/MainActivity.java
+++ app/src/main/java/micromaster/beginner/com/mytodolist/MainActivity.java
@@ -1,6 +1,10 @@
 package micromaster.beginner.com.mytodolist;
 
+import android.database.Cursor;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
@@ -8,7 +12,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import micromaster.beginner.com.mytodolist.data.TodoListContrat;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,28 @@ public class MainActivity extends AppCompatActivity {
         actionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO
+                AddTaskDialogFragment dialogFragment = new AddTaskDialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "addTask");
             }
         });
+
+        getSupportLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, TodoListContrat.TodoEntry.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if(data != null){
+
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        loader.cancelLoad();
     }
 }
